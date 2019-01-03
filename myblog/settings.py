69fa77c160b87b74105c9824d7bbdc290e343cb6
ghostry <ghostry@ghostry.cn>
2019-01-03 22:40:08 +0800

    up

--- myblog/settings.py
+++ myblog/settings.py
@@ -120,6 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 STATICFILES_DIRS = ( os.path.join('static'), )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
